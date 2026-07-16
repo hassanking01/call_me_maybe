26f79fab022f142a77d50ff3e8bb11911f1ff10d
hassan <hassanahchtar953@gmail.com>
2026-07-16 09:59:15 +0100

    [ADDED] model class the keep eveything together

--- src/__main__.py
+++ src/__main__.py
@@ -1,75 +1,18 @@
+from src.utils import Model, Parser
 from rich.traceback import install
 install()
-from llm_sdk import Small_LLM_Model
-import json
-from src.utils import Function, paramType, Parser, 
 
-parser = Parser()
+def main():
+    parser = Parser()
+    model = Model(parser)
+    model.run()
 
-
-
-
-
-fun_str = ""
-
-
-
-base_prompt = (
-"<|im_start|>system\n"
-"You are a function calling system. Given a user request, "
-"output ONLY a JSON object calling the correct "
-"function, in this exact format:\n"
-'{"name": "function_name", "parameters": {...}}\n'
-"Available functions:\n"
-f"{fun_str}"
-"<|im_end|>\n"
-"<|im_start|>user\n"
-)
-
-
-
-
-def collect_tokens(state, token, allowed_tokens):
-    if state == -1:
-        return
-    if state not in fsm.tier:
-        return
-    for ch in fsm.tier[state]:
-        new_token = token + ch
-        new_state = fsm.tier[state][ch]
-        if new_token in set_data:
-            allowed_tokens += [decoded_data[new_token]]
-            collect_tokens(new_state, new_token, allowed_tokens) 
-
-
-try:
-    for promt in prompts:
-        final_prompt = base_prompt + promt
-        final_prompt += "<|im_end|>" 
-        final_prompt += "\n" + "<|im_start|>assistant"        
-        ids = model.encode(final_prompt).tolist()[0]
-        line = ""
-        resutl = []
-        while True:
-            allowed_tokens = []
-            state = fsm.current_state
-            collect_tokens(fsm.current_state, "" ,allowed_tokens)
-            logits = model.get_logits_from_input_ids(ids)
-            filtred_logits = [float("-inf")] * len(logits)
-            for token_id in allowed_tokens:
-                filtred_logits[token_id] = logits[token_id]
-            logits = filtred_logits
-            m = logits.index(max(logits))
-            ids += [m]
-            line += data[m]
-
-            print(data[m], end="", flush=True)
-            if not fsm.update_state(data[m]):
-                fsm.current_state = 0
-                valid_json = json.loads(line)
-                resutl += [{"prompt": promt, **valid_json}]
-                print()
-                break
-    print(resutl)
-except KeyboardInterrupt:
-    pass
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
+    except Exception as e:
+        print(e)
+    except BaseException as e:
+        print(e)
--- src/utils.py
+++ src/utils.py
@@ -99,7 +99,9 @@ class myfsm:
                     case paramType.INTEGER:
                         pass
                     case paramType.STRING:
-                        pass
+                        self.tier.setdefault(self.state, {}).update({'"': self.state + 1})
+                        self.tier.setdefault(self.state + 1, {}).update({None: self.state + 2})
+                        self.state += 2
                     case paramType.BOOLEAN:
                         pass
                 
@@ -112,12 +114,16 @@ class myfsm:
                     self.state += 1
                     self.tier.setdefault(self.state, {}).update({"}": -1})
 
-    def update_state(self, generated_token: str):
-        for c in generated_token:
-            if c in self.tier[self.current_state]:
-                self.current_state =  self.tier[self.current_state][c]
-                if self.current_state == -1:
-                    return False
+    def update_state(self, generated_token: str, allowed_tokens):
+        if allowed_tokens:
+            for c in generated_token:
+                if c in self.tier[self.current_state]:
+                    self.current_state =  self.tier[self.current_state][c]
+                    if self.current_state == -1:
+                        return False
+        else:
+            if '"' in generated_token:
+                self.current_state = self.tier[self.current_state][None]
         return True
 
 class Model:
@@ -131,6 +137,34 @@ class Model:
         self.get_prompts()
         self.fsm.create_tier(self.functions)
         self.functions_str = ""
+        self._creat_str_functions()
+        self.base_prompt = (
+            "<|im_start|>system\n"
+            "You are a function calling system. Given a user request, "
+            "output ONLY a JSON object calling the correct "
+            "function, in this exact format:\n"
+            '{"name": "function_name", "parameters": {...}}\n'
+            "Available functions:\n"
+            f"{self.functions_str}"
+            "<|im_end|>\n"
+            "<|im_start|>user\n"
+            )
+        print(self.base_prompt)
+        exit()
+        self.set_data = set()
+        self.data = {}
+        self.decoded_data = {}
+        self.get_vocabulary()
+        self.final_result = []
+    
+    def get_vocabulary(self):
+        with open(self.model.get_path_to_vocab_file()) as vocab_file:
+            bad_vocab_json = json.load(vocab_file)
+        for key in bad_vocab_json:
+            token = self.model.decode(bad_vocab_json[key])
+            self.decoded_data[token] = bad_vocab_json[key]
+            self.data[bad_vocab_json[key]] = token
+            self.set_data.add(token)
 
     def _creat_str_functions(self):
         for function in self.functions:
@@ -152,6 +186,20 @@ class Model:
                 }
             )]
 
+    def collect_tokens(self, state, token, allowed_tokens):
+        if state == -1:
+            return
+        if state not in self.fsm.tier:
+            return
+        for ch in self.fsm.tier[state]:
+            if not ch:
+                return
+            new_token = token + ch
+            new_state = self.fsm.tier[state][ch]
+            if new_token in self.set_data:
+                allowed_tokens += [self.decoded_data[new_token]]
+                self.collect_tokens(new_state, new_token, allowed_tokens) 
+
     def save_output(self):
         pass
 
@@ -159,3 +207,31 @@ class Model:
         with  open(self.parser.input) as f:
             data = json.load(f)
             self.prompts = [key['prompt'] for key in data ]
+
+    def run(self):
+        for promt in self.prompts:
+            final_prompt = self.base_prompt + promt
+            final_prompt += "<|im_end|>" 
+            final_prompt += "\n" + "<|im_start|>assistant"        
+            ids = self.model.encode(final_prompt).tolist()[0]
+            line = ""
+            while True:
+                allowed_tokens = []
+                self.collect_tokens(self.fsm.current_state, "" ,allowed_tokens)
+                logits = self.model.get_logits_from_input_ids(ids)
+                filtred_logits = [float("-inf")] * len(logits)
+                if allowed_tokens:
+                    for token_id in allowed_tokens:
+                        filtred_logits[token_id] = logits[token_id]
+                    logits = filtred_logits
+                m = logits.index(max(logits))
+                ids += [m]
+                line += self.data[m]
+
+                print(self.data[m], end="", flush=True)
+                if not self.fsm.update_state(self.data[m], allowed_tokens):
+                    self.fsm.current_state = 0
+                    valid_json = json.loads(line)
+                    self.final_result += [{"prompt": promt, **valid_json}]
+                    print()
+                    break
